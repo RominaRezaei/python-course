34fb390aa93051c99239cae90ec2f88bef880a0d
RominaRezaei <140347133+RominaRezaei@users.noreply.github.com>
2023-07-31 18:47:53 +0330

    Add files via upload

---
+++ py3 homework.py
@@ -0,0 +1,11 @@
+import random
+roll =[]
+x = random.randint(1,6)
+roll.append(x)
+print(roll)
+
+while roll == 6:
+  roll_again=[]
+  y= random.randint(1, 6)
+  roll_again.append(y)
+  print (roll_again)
